763b8b7872a46060dc47dd58856dcde23ea8d1ee
jinjianbing <jinjianbing@jingshuo.com>
2019-12-04 10:20:23 +0800

    add model

--- MyModel.py
+++ MyModel.py
@@ -27,7 +27,7 @@ class MyModel(object):
     """
     Model template. You can load your model parameters in __init__ from a location accessible at runtime
     """
-    def __init__(self, fix = 2, url = 'https://shield.mlamp.cn/task/api/file/space/download/8ee73d61af60d387963f3c4badc4b2af/56504/deepfm_baseline_v0'):
+    def __init__(self, fix = 2, url = 'https://shield.mlamp.cn/task/api/file/space/download/b3b84baf1fd59ca01bb3d2e95cde70fe/60288/model.m'):
         """
         Add any initialization parameters. These will be passed at runtime from the graph definition parameters defined in your seldondeployment kubernetes resource manifest.
         """
@@ -36,15 +36,15 @@ class MyModel(object):
         self.url = url
         self.loaded = False
         self.model = None
-        #self.load()
 
     def load(self):
         print("start download")
         print(self.url)
-        urllib.request.urlretrieve(self.url, "model.m")
+        #urllib.request.urlretrieve(self.url, "model.m")
         print("start loading model")
-        self.model = torch.load('model.m', map_location=torch.device('cpu'))
+        #self.model = torch.load('model.m', map_location=torch.device('cpu'))
         print("model loaded")
+
     def predict(self, X, features_names=None):
         """
         Return a prediction.
@@ -54,11 +54,16 @@ class MyModel(object):
         X : array-like
         feature_names : array of feature names (optional)
         """
+        if not self.loaded:
+            self.load()
+
+        print(X)
         if self.model:
-            x1, x2 = self.trans(X)
-            t0 = torch.tensor(x1)
-            t1 = torch.tensor(x2)
-            return [torch.sigmoid(self.model(t0, t1)).data.item()]
+            # x1, x2 = self.trans(X)
+            # t0 = torch.tensor(x1)
+            # t1 = torch.tensor(x2)
+            # return [torch.sigmoid(self.model(t0, t1)).data.item()]
+            return "1"
         else:
             return "less is more more more more %d" % self.fix
 
@@ -72,8 +77,8 @@ class MyModel(object):
         return (xi, xv)
 
 
-#tt =  [106028, 195092, 10568, 23, 147, 4, 11, 1241, 6.09887064e-01, -3.17003161e-01, -1.83307350e-01, -4.45917211e-02, -4.00365591e-02,  2.60544335e-03, -2.43274420e-02, -1.35902567e-02, -2.06686687e-02, -2.39776302e-04, -8.98106117e-03, -1.32717369e-02, -9.00286250e-03, -9.20017343e-03, -1.12582045e-02, -9.56592243e-03, -5.72999334e-03, -3.99997272e-03, -9.94744524e-03, -6.57328777e-03, -4.06617252e-03, -7.16522615e-03, -3.39697767e-03, -5.05888509e-03, -6.38805423e-03, -6.68853614e-03, -6.55218540e-03, -3.32565443e-03, -7.25812372e-03, -9.18245874e-04,  1.18093006e-03, 3.55028023e-04, -4.88233333e-03, -1.80893322e-03, -3.13342735e-03, -3.14912642e-03, -4.47223382e-03, 8.49320320e-04, -3.30703938e-03, -3.95207189e-06, -3.04178707e-03, -3.35240504e-03, -2.29544588e-03, -2.08881940e-03, -1.75165117e-03, -2.58994359e-03, 5.19961119e-04, -3.13837733e-03, -3.30228242e-03, 3.50067829e-04 ]
-
-#aa = MyModel()
-#print(aa.predict(tt))
+# tt =  [106028, 195092, 10568, 23, 147, 4, 11, 1241, 6.09887064e-01, -3.17003161e-01, -1.83307350e-01, -4.45917211e-02, -4.00365591e-02,  2.60544335e-03, -2.43274420e-02, -1.35902567e-02, -2.06686687e-02, -2.39776302e-04, -8.98106117e-03, -1.32717369e-02, -9.00286250e-03, -9.20017343e-03, -1.12582045e-02, -9.56592243e-03, -5.72999334e-03, -3.99997272e-03, -9.94744524e-03, -6.57328777e-03, -4.06617252e-03, -7.16522615e-03, -3.39697767e-03, -5.05888509e-03, -6.38805423e-03, -6.68853614e-03, -6.55218540e-03, -3.32565443e-03, -7.25812372e-03, -9.18245874e-04,  1.18093006e-03, 3.55028023e-04, -4.88233333e-03, -1.80893322e-03, -3.13342735e-03, -3.14912642e-03, -4.47223382e-03, 8.49320320e-04, -3.30703938e-03, -3.95207189e-06, -3.04178707e-03, -3.35240504e-03, -2.29544588e-03, -2.08881940e-03, -1.75165117e-03, -2.58994359e-03, 5.19961119e-04, -3.13837733e-03, -3.30228242e-03, 3.50067829e-04 ]
+#
+# aa = MyModel()
+# print(aa.predict(tt))
 
