bf749004a9997abb193530ffc7d7d20a17a865c7
jinjianbing <jinjianbing@jingshuo.com>
2020-03-10 16:04:54 +0800

    add version

--- MyModel.py
+++ MyModel.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 import torch.backends.cudnn
+import connect_s3
 import transform
 import network
 from importlib import reload
@@ -10,22 +11,21 @@ class MyModel(object):
     """
     Model template. You can load your model parameters in __init__ from a location accessible at runtime
     """
-    def __init__(self):
+    def __init__(self, bucket, model_key, network_key, transform_key=None):
         """
         Add any initialization parameters. These will be passed at runtime from the graph definition parameters defined in your seldondeployment kubernetes resource manifest.
         """
         print("Initializing")
+        print("bucket: " + bucket)
+        print("model key: " + model_key)
         self.loaded = False
-        # print("bucket: " + bucket)
-        # print("model key: " + model_key)
-        # self.loaded = False
-        # self.model = None
-        # self.s3 = connect_s3.ConnectS3(bucket, model_key, transform_key, network_key)
-        # if not transform_key is None:
-        #     print("transform key: " + transform_key)
-        #     reload(transform)
-        #
-        # reload(network)
+        self.model = None
+        self.s3 = connect_s3.ConnectS3(bucket, model_key, network_key, transform_key)
+        if not transform_key is None:
+            print("transform key: " + transform_key)
+            reload(transform)
+        self.__version = self.s3.get_version()
+        reload(network)
 
     def load(self):
         print("start load model")
@@ -47,14 +47,17 @@ class MyModel(object):
 
         if self.model:
             transform_obj = transform.Transform()
-            flag, msg = transform_obj.transform_input(X)
-            if flag:
-                return msg
+            transform_obj.transform_input(X)
             return transform_obj.transform_output(self.model)
         else:
             return "less is more more more more"
-#tt= [4, 57, [106028, 10568, 23, 147, 4, 11, 1241, 6.09887064e-01, -3.17003161e-01, -1.83307350e-01, -4.45917211e-02, -4.00365591e-02,  2.60544335e-03, -2.43274420e-02, -1.35902567e-02, -2.06686687e-02, -2.39776302e-04, -8.98106117e-03, -1.32717369e-02, -9.00286250e-03, -9.20017343e-03, -1.12582045e-02, -9.56592243e-03, -5.72999334e-03, -3.99997272e-03, -9.94744524e-03, -6.57328777e-03, -4.06617252e-03, -7.16522615e-03, -3.39697767e-03, -5.05888509e-03, -6.38805423e-03, -6.68853614e-03, -6.55218540e-03, -3.32565443e-03, -7.25812372e-03, -9.18245874e-04,  1.18093006e-03, 3.55028023e-04, -4.88233333e-03, -1.80893322e-03, -3.13342735e-03, -3.14912642e-03, -4.47223382e-03, 8.49320320e-04, -3.30703938e-03, -3.95207189e-06, -3.04178707e-03, -3.35240504e-03, -2.29544588e-03, -2.08881940e-03, -1.75165117e-03, -2.58994359e-03, 5.19961119e-04, -3.13837733e-03, -3.30228242e-03, 3.50067829e-04 ], 3, [195092, 195093, 195094]]
-#aa = MyModel()
-#tt = np.array(tt)
-#print(aa.predict(tt))
+
+    def tags(self):
+        return {"version": self.__version}
+
+# tt= [2, 57, [106028, 10568, 23, 147, 4, 11, 1241, 6.09887064e-01, -3.17003161e-01, -1.83307350e-01, -4.45917211e-02, -4.00365591e-02,  2.60544335e-03, -2.43274420e-02, -1.35902567e-02, -2.06686687e-02, -2.39776302e-04, -8.98106117e-03, -1.32717369e-02, -9.00286250e-03, -9.20017343e-03, -1.12582045e-02, -9.56592243e-03, -5.72999334e-03, -3.99997272e-03, -9.94744524e-03, -6.57328777e-03, -4.06617252e-03, -7.16522615e-03, -3.39697767e-03, -5.05888509e-03, -6.38805423e-03, -6.68853614e-03, -6.55218540e-03, -3.32565443e-03, -7.25812372e-03, -9.18245874e-04,  1.18093006e-03, 3.55028023e-04, -4.88233333e-03, -1.80893322e-03, -3.13342735e-03, -3.14912642e-03, -4.47223382e-03, 8.49320320e-04, -3.30703938e-03, -3.95207189e-06, -3.04178707e-03, -3.35240504e-03, -2.29544588e-03, -2.08881940e-03, -1.75165117e-03, -2.58994359e-03, 5.19961119e-04, -3.13837733e-03, -3.30228242e-03, 3.50067829e-04 ], 2, [195092, 195093]]
+# aa = MyModel("kubeflow-anonymous-test", "pytorch/2020-03-10T14-17-29Z/pytorch", "pytorch/2020-03-10T14-17-29Z/network")
+# tt = np.array(tt)
+# print(aa.predict(tt))
+# print(aa.tags())
 
--- model.m
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4d63d625be618988ee205dcbf753a9abb0b056c8538344d41f047bfcf4e7ff53
-size 105518924
